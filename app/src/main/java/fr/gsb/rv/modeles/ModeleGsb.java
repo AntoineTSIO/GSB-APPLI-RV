18a9729f7c6ba924879ee7acd9877e5bca1a9990
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-03-07 12:02:14 +0100

    V0.1.4

--- app/src/main/java/fr/gsb/rv/modeles/ModeleGsb.java
+++ app/src/main/java/fr/gsb/rv/modeles/ModeleGsb.java
@@ -1,4 +1,28 @@
 package fr.gsb.rv.modeles;
 
+import fr.gsb.rv.entites.Visiteur;
+
 public class ModeleGsb {
+
+    private ModeleGsb modele = null;
+
+    private void ModeleGsb(){
+        modele = new ModeleGsb();
+    }
+
+    public ModeleGsb getInstance(){
+        return modele;
+    }
+
+    public Visiteur seConnecter(String matricule, String mdp){
+        String okMdp = "azerty";
+        String okMatricule = "a131";
+
+        if(matricule.equals(okMatricule) && mdp.equals(okMdp)) {
+            Visiteur vis = new Visiteur("a131", "azerty", "Edouard", "Jean");
+            return vis;
+        }
+        return null;
+    }
+
 }
