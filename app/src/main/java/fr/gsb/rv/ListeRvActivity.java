5f5c6c9db410ead3abaf0336a2e8bd63c4b4cb3c
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-03-31 16:47:55 +0200

    Ajout des différentes activités

---
+++ app/src/main/java/fr/gsb/rv/ListeRvActivity.java
@@ -0,0 +1,14 @@
+package fr.gsb.rv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeRvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_rv);
+    }
+}
