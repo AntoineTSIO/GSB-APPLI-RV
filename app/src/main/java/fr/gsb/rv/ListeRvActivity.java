b465ea3829f6fa2e23457ca1741834a1d97cc828
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-04-02 17:24:25 +0200

    Suite UC2 - Consulter un rapport de visite

--- app/src/main/java/fr/gsb/rv/ListeRvActivity.java
+++ app/src/main/java/fr/gsb/rv/ListeRvActivity.java
@@ -10,5 +10,7 @@ public class ListeRvActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_rv);
+
+
     }
 }
