4d2c15340ad96bec6841026540de614c77d69b2b
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-04-03 13:11:58 +0200

    Ajout des entités RapportVisite et Praticien

---
+++ app/src/main/java/fr/gsb/rv/entites/Praticien.java
@@ -0,0 +1,131 @@
+package fr.gsb.rv.entites;
+
+import java.time.LocalDate;
+
+public class Praticien {
+
+    private int numero ;
+    private String nom ;
+    private String prenom ;
+    private String adresse ;
+    private String codePostal ;
+    private String ville ;
+    private double coefNotoriete ;
+    private LocalDate dateDerniereVisite ;
+    private int dernierCoefConfiance ;
+
+    public Praticien(int pra_num, String pra_nom, String pra_ville, double pra_coefnotoriete, LocalDate rap_date_visite,
+                     int rap_coef_confiance) {
+        this.numero = pra_num;
+        this.nom = pra_nom;
+        this.ville = pra_ville;
+        this.coefNotoriete = pra_coefnotoriete;
+        this.dateDerniereVisite = rap_date_visite;
+        this.dernierCoefConfiance = rap_coef_confiance;
+    }
+
+    public Praticien(int numero, String nom, String prenom, String adresse, String codePostal, String ville, double coefNotoriete, LocalDate dateDerniereVisite, int dernierCoefConfiance) {
+        this.numero = numero;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.codePostal = codePostal;
+        this.ville = ville;
+        this.coefNotoriete = coefNotoriete;
+        this.dateDerniereVisite = dateDerniereVisite;
+        this.dernierCoefConfiance = dernierCoefConfiance;
+    }
+
+    public Praticien(int pra_num, String pra_nom, String pra_ville) {
+        this.numero = pra_num;
+        this.nom = pra_nom;
+        this.ville = pra_ville;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return this.adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCodePostal() {
+        return this.codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return this.ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public double getCoefNotoriete() {
+        return this.coefNotoriete;
+    }
+
+    public void setCoefNotoriete(double coefNotoriete) {
+        this.coefNotoriete = coefNotoriete;
+    }
+
+    public LocalDate getDateDerniereVisite() {
+        return this.dateDerniereVisite;
+    }
+
+    public void setDateDerniereVisite(LocalDate dateDerniereVisite) {
+        this.dateDerniereVisite = dateDerniereVisite;
+    }
+
+    public int getDernierCoefConfiance() {
+        return this.dernierCoefConfiance;
+    }
+
+    public void setDernierCoefConfiance(int dernierCoefConfiance) {
+        this.dernierCoefConfiance = dernierCoefConfiance;
+    }
+
+    @Override
+    public String toString() {
+        return "Praticien{" +
+                "numero=" + numero +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", codePostal='" + codePostal + '\'' +
+                ", ville='" + ville + '\'' +
+                ", coefNotoriete=" + coefNotoriete +
+                ", dateDerniereVisite=" + dateDerniereVisite +
+                ", dernierCoefConfiance=" + dernierCoefConfiance +
+                '}';
+    }
+}
---
+++ app/src/main/java/fr/gsb/rv/entites/RapportVisite.java
@@ -0,0 +1,130 @@
+package fr.gsb.rv.entites;
+
+import java.time.LocalDate;
+
+public class RapportVisite {
+
+    private int numero ;
+    private LocalDate dateVisite ;
+    private LocalDate dateRedaction ;
+    private String bilan ;
+    private String motif ;
+    private int coefConfiance ;
+    private boolean lu ;
+    private Visiteur visiteur ;
+    private Praticien praticien ;
+
+    public RapportVisite(int numero, LocalDate dateVisite, LocalDate dateRedaction, String bilan, String motif, int coefConfiance, boolean lu, Visiteur visiteur, Praticien praticien) {
+        this.numero = numero;
+        this.dateVisite = dateVisite;
+        this.dateRedaction = dateRedaction;
+        this.bilan = bilan;
+        this.motif = motif;
+        this.coefConfiance = coefConfiance;
+        this.lu = lu;
+        this.visiteur = visiteur ;
+        this.praticien = praticien ;
+    }
+
+    public RapportVisite(int rap_num, LocalDate rap_date_visite, LocalDate rap_date_saisie, String rap_bilan, String rap_motif, int rap_coef_confiance, boolean rap_lu, Visiteur vis_matricule) {
+        this.numero = rap_num;
+        this.dateVisite = rap_date_visite;
+        this.dateRedaction = rap_date_saisie;
+        this.bilan = rap_bilan;
+        this.motif = rap_motif;
+        this.coefConfiance = rap_coef_confiance;
+        this.lu = rap_lu;
+        this.visiteur = vis_matricule ;
+    }
+
+    public RapportVisite() {
+
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getDateVisite() {
+        return this.dateVisite;
+    }
+
+    public void setDateVisite(LocalDate dateVisite) {
+        this.dateVisite = dateVisite;
+    }
+
+    public LocalDate getDateRedaction() {
+        return this.dateRedaction;
+    }
+
+    public void setDateRedaction(LocalDate dateRedaction) {
+        this.dateRedaction = dateRedaction;
+    }
+
+    public String getBilan() {
+        return this.bilan;
+    }
+
+    public void setBilan(String bilan) {
+        this.bilan = bilan;
+    }
+
+    public String getMotif() {
+        return this.motif;
+    }
+
+    public void setMotif(String motif) {
+        this.motif = motif;
+    }
+
+    public int getCoefConfiance() {
+        return this.coefConfiance;
+    }
+
+    public void setCoefConfiance(int coefConfiance) {
+        this.coefConfiance = coefConfiance;
+    }
+
+    public boolean isLu() {
+        return this.lu;
+    }
+
+    public void setLu(boolean lu) {
+        this.lu = lu;
+    }
+
+    public Visiteur getVisiteur() {
+        return visiteur;
+    }
+
+    public void setVisiteur(Visiteur visiteur) {
+        this.visiteur = visiteur;
+    }
+
+    public Praticien getPraticien() {
+        return praticien;
+    }
+
+    public void setPraticien(Praticien praticien) {
+        this.praticien = praticien;
+    }
+
+    @Override
+    public String toString() {
+        return "RapportVisite{" +
+                "numero=" + numero +
+                ", dateVisite=" + dateVisite +
+                ", dateRedaction=" + dateRedaction +
+                ", bilan='" + bilan + '\'' +
+                ", motif='" + motif + '\'' +
+                ", coefConfiance=" + coefConfiance +
+                ", lu=" + lu +
+                ", visiteur=" + visiteur +
+                ", praticien=" + praticien +
+                '}';
+    }
+}
