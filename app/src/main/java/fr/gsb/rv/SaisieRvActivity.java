9c038815d6c914d799ccf3052ff9fa65cd077730
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-04-06 21:24:22 +0200

    Début implémentation UC3 - Saisir un rapport de visite

--- app/src/main/java/fr/gsb/rv/SaisieRvActivity.java
+++ app/src/main/java/fr/gsb/rv/SaisieRvActivity.java
@@ -3,12 +3,32 @@ package fr.gsb.rv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SaisieRvActivity extends AppCompatActivity {
+
+    TextView tvDateVisite;
+    Button bDateVisite;
+    Spinner spPraticien;
+    Spinner spMotif;
+    EditText etBilan;
+    Spinner spCoefConfiance;
+    Button bValider;
+    Button bAnnuler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisie_rv);
+
+        tvDateVisite = findViewById(R.id.tvDateVisite);
+        spPraticien = findViewById(R.id.spPraticien);
+        spMotif = findViewById(R.id.spMotif);
+        spCoefConfiance = findViewById(R.id.spCoefConfiance);
+        etBilan = findViewById(R.id.etBilan);
     }
 }
